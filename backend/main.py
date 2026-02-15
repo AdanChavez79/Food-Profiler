dea5f030d9eb920ced4fb66c99d0b2fbb4d9082e
Alenz81 <superblender200@gmail.com>
2026-02-14 22:50:37 -0800

    Added api calls for adding csv info and deleting the database
    Added csv file (the dataset), as well as api calls to delete everything in the database (except users at the moment), and to populate the database from the aforementioned csv file

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,8 @@
+import json
+import numpy as np
+import pandas as pd
+from typing import List, Dict
+from collections import defaultdict
 from fastapi import FastAPI
 import asyncpg
 import os
@@ -9,6 +14,23 @@ load_dotenv()
 app = FastAPI()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+def token_positions(tokens: List[str]) -> Dict[str, List[int]]:
+    pos = defaultdict(list)
+    for i, t in enumerate(tokens):
+        pos[t].append(i)
+    return dict(pos)
+
+def build_inverted_index_simple(df: pd.DataFrame) -> Dict[str, List[int]]:
+    idx: Dict[str, List[int]] = defaultdict(list)
+    for row in df.itertuples(index=False):
+        food_id = int(row.Index)
+        pos_map = row.pos  # dict term -> [positions]
+        for term, positions in pos_map.items():
+            idx[term].append(food_id)
+    for term in idx:
+        idx[term].sort(key=lambda p: p)
+    return dict(idx)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -112,6 +134,106 @@ async def get_user_allergies():
                 """            
                 )
             return rows
+    except Exception as e:
+        return {"error": str(e)}
+    
+
+
+@app.delete("/delete_all_rows_from_database")
+async def delete_all_rows_from_database():
+    try:
+        async with app.state.pool.acquire() as conn:
+            await conn.execute(
+                """
+                TRUNCATE TABLE user_preferences_ingredients, user_preferences_meals, user_allergies, meal_ingredients, meals, ingredients RESTART IDENTITY CASCADE;
+                """, #, users add this back if you want to delete all users as well
+            )
+            return {"message": "All rows deleted successfully"}
+    except Exception as e:
+        return {"error": str(e)}
+    
+
+@app.post("/populate_database") #might need to cast data as string before adding to database
+async def populate_database():
+    try:
+        async with app.state.pool.acquire() as conn:
+            database_pd = (pd.read_csv('recipes_final.csv')).fillna(0)
+            database_pd.rename(columns={'Unnamed: 0': 'Index'}, inplace=True)
+            database_pd["Index"] = database_pd.index
+            
+            #column_name = "RecipeIngredientParts" #partially tokenized
+            column_name = "RecipeIngredientParts_clean_list" #heavily tokenized
+
+
+            database_pd[column_name] = database_pd[column_name].map(str)
+            database_pd[column_name] = database_pd[column_name].replace("',", '",', regex=True)
+            database_pd[column_name] = database_pd[column_name].replace(", '", ', "', regex=True)
+            database_pd[column_name] = database_pd[column_name].replace("']", '"]', regex=True)
+            database_pd[column_name] = database_pd[column_name].replace(r"\['", '["', regex=True)
+            
+
+            database_pd["tokens"] = database_pd[column_name].map(json.loads)
+            #database_pd["tokens"] = database_pd["tokens"].map(np.sort)
+
+            database_pd["pos"] = database_pd["tokens"].map(token_positions)
+
+            inverted_index = build_inverted_index_simple(database_pd)
+
+            ingredient_rows = []
+            meal_ingredient_rows = []
+            meal_rows = []
+
+
+            for index, row in database_pd.iterrows():
+                meal_rows.append((
+                    str(row["Images"]),
+                    str(row["Name"]),
+                    str(row["RecipeInstructions"]),
+                    str(row["RecipeCategory"]),
+                    str(row["Description"]),
+                    float(row["AggregatedRating"]),
+                    float(row["Calories"]),
+                    int(row["RecipeServings"]),
+                    str(row["RecipeYield"]),
+                    int(row["hours"]),
+                    int(row["minutes"]),
+                    int(row["totaltime_min"]),
+                    str(row["calories_classification"]),
+                    str(row["MacroClassification"])
+                ))
+
+            await conn.executemany(
+                """
+                INSERT INTO meals (images, name, recipe_instructions, recipe_category, description, aggregated_rating, calories, recipe_servings, recipe_yield, hours, minutes, totaltime_min, calories_classification, macro_classification)
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+                """,
+                meal_rows
+            )
+
+
+            for index, (key, value) in enumerate(inverted_index.items()):
+                ingredient_rows.append((str(key), str(value)))
+
+                for meal_id in value:
+                    meal_ingredient_rows.append((int(meal_id + 1), int(index + 1), str(database_pd.loc[meal_id, "RecipeServings"])))
+
+            await conn.executemany(
+                """
+                INSERT INTO ingredients (name, meals)
+                VALUES ($1, $2)
+                """,
+                ingredient_rows
+            )
+
+            await conn.executemany(
+                """
+                INSERT INTO meal_ingredients (meal_id, ingredient_id, serving)
+                VALUES ($1, $2, $3)
+                """,
+                meal_ingredient_rows
+            )
+            
+            return {"message": "Database populated successfully"}
     except Exception as e:
         return {"error": str(e)}
     
