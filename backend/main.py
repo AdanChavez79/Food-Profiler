d05fb3eacc1c49fad5a3a0f30929ecd77f712028
AdanChavez79 <adan7chavez9@gmail.com>
2026-02-15 16:43:37 -0800

    Recommendation Query Functions

--- backend/main.py
+++ backend/main.py
@@ -140,104 +140,50 @@ async def get_user_allergies():
     
 
 
-@app.delete("/delete_all_rows_from_database")
-async def delete_all_rows_from_database():
-    try:
-        async with app.state.pool.acquire() as conn:
-            await conn.execute(
-                """
-                TRUNCATE TABLE user_preferences_ingredients, user_preferences_meals, user_allergies, meal_ingredients, meals, ingredients RESTART IDENTITY CASCADE;
-                """, #, users add this back if you want to delete all users as well
+async def get_ingredient_meal_list(ingredient_id: int):
+    async with app.state.pool.acquire() as conn:
+        rows = await conn.fetch(
+            """
+            SELECT i.meals
+            FROM ingredients i
+            WHERE i.id = $1;
+            """,
+            ingredient_id            
             )
-            return {"message": "All rows deleted successfully"}
-    except Exception as e:
-        return {"error": str(e)}
+        return rows
     
-
-@app.post("/populate_database") #might need to cast data as string before adding to database
-async def populate_database():
-    try:
-        async with app.state.pool.acquire() as conn:
-            database_pd = (pd.read_csv('recipes_final.csv')).fillna(0)
-            database_pd.rename(columns={'Unnamed: 0': 'Index'}, inplace=True)
-            database_pd["Index"] = database_pd.index
-            
-            #column_name = "RecipeIngredientParts" #partially tokenized
-            column_name = "RecipeIngredientParts_clean_list" #heavily tokenized
-
-
-            database_pd[column_name] = database_pd[column_name].map(str)
-            database_pd[column_name] = database_pd[column_name].replace("',", '",', regex=True)
-            database_pd[column_name] = database_pd[column_name].replace(", '", ', "', regex=True)
-            database_pd[column_name] = database_pd[column_name].replace("']", '"]', regex=True)
-            database_pd[column_name] = database_pd[column_name].replace(r"\['", '["', regex=True)
-            
-
-            database_pd["tokens"] = database_pd[column_name].map(json.loads)
-            #database_pd["tokens"] = database_pd["tokens"].map(np.sort)
-
-            database_pd["pos"] = database_pd["tokens"].map(token_positions)
-
-            inverted_index = build_inverted_index_simple(database_pd)
-
-            ingredient_rows = []
-            meal_ingredient_rows = []
-            meal_rows = []
-
-
-            for index, row in database_pd.iterrows():
-                meal_rows.append((
-                    str(row["Images"]),
-                    str(row["Name"]),
-                    str(row["RecipeInstructions"]),
-                    str(row["RecipeCategory"]),
-                    str(row["Description"]),
-                    float(row["AggregatedRating"]),
-                    float(row["Calories"]),
-                    int(row["RecipeServings"]),
-                    str(row["RecipeYield"]),
-                    int(row["hours"]),
-                    int(row["minutes"]),
-                    int(row["totaltime_min"]),
-                    str(row["calories_classification"]),
-                    str(row["MacroClassification"])
-                ))
-
-            await conn.executemany(
-                """
-                INSERT INTO meals (images, name, recipe_instructions, recipe_category, description, aggregated_rating, calories, recipe_servings, recipe_yield, hours, minutes, totaltime_min, calories_classification, macro_classification)
-                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
-                """,
-                meal_rows
+async def get_meal(meal_id: int):
+    async with app.state.pool.acquire() as conn:
+        name = await conn.fetchval(
+            """
+            SELECT m.name
+            FROM meals m
+            WHERE m.id = $1;
+            """,
+            meal_id            
             )
+        return name
 
-
-            for index, (key, value) in enumerate(inverted_index.items()):
-                ingredient_rows.append((str(key), str(value)))
-
-                for meal_id in value:
-                    meal_ingredient_rows.append((int(meal_id + 1), int(index + 1), str(database_pd.loc[meal_id, "RecipeServings"])))
-
-            await conn.executemany(
-                """
-                INSERT INTO ingredients (name, meals)
-                VALUES ($1, $2)
-                """,
-                ingredient_rows
-            )
-
-            await conn.executemany(
-                """
-                INSERT INTO meal_ingredients (meal_id, ingredient_id, serving)
-                VALUES ($1, $2, $3)
-                """,
-                meal_ingredient_rows
-            )
-            
-            return {"message": "Database populated successfully"}
-    except Exception as e:
-        return {"error": str(e)}
     
+async def get_meal_ingredients(meal_id: int):
+    async with app.state.pool.acquire() as conn:
+        rows = await conn.fetch(
+            """
+            SELECT i.name
+            FROM meal_ingredients mi
+            JOIN ingredients i ON i.id = mi.ingredient_id 
+            WHERE mi.meal_id = $1;
+            """,
+            meal_id            
+            )
+        return rows
+
+
+async def get_num_meals():
+    async with app.state.pool.acquire() as conn:
+        return await conn.fetchval("SELECT COUNT(*) FROM meals;")
+   
+
 
 """
 def reccomend_food(profile: List[Tuple[str, int]], inv: dict, database_list: np.ndarray, matches_ingredients: List[List[Tuple[str, int]]]) -> Tuple[List[int], np.ndarray, List[List[str]]]:
@@ -291,3 +237,93 @@ def run_and_print_reccomendations():
     end_time = time()
     print(f"Execution time: {end_time - start_time} seconds")
     """
+
+
+
+# async def delete_all_rows_from_database():
+#     async with app.state.pool.acquire() as conn:
+#         await conn.execute(
+#             """
+#             TRUNCATE TABLE user_preferences_ingredients, user_preferences_meals, user_allergies, meal_ingredients, meals, ingredients RESTART IDENTITY CASCADE;
+#             """, #, users add this back if you want to delete all users as well
+#         )
+    
+
+# async def populate_database():
+#     async with app.state.pool.acquire() as conn:
+#         database_pd = (pd.read_csv('recipes_final.csv')).fillna(0)
+#         database_pd.rename(columns={'Unnamed: 0': 'Index'}, inplace=True)
+#         database_pd["Index"] = database_pd.index
+        
+#         #column_name = "RecipeIngredientParts" #partially tokenized
+#         column_name = "RecipeIngredientParts_clean_list" #heavily tokenized
+
+
+#         database_pd[column_name] = database_pd[column_name].map(str)
+#         database_pd[column_name] = database_pd[column_name].replace("',", '",', regex=True)
+#         database_pd[column_name] = database_pd[column_name].replace(", '", ', "', regex=True)
+#         database_pd[column_name] = database_pd[column_name].replace("']", '"]', regex=True)
+#         database_pd[column_name] = database_pd[column_name].replace(r"\['", '["', regex=True)
+        
+
+#         database_pd["tokens"] = database_pd[column_name].map(json.loads)
+#         #database_pd["tokens"] = database_pd["tokens"].map(np.sort)
+
+#         database_pd["pos"] = database_pd["tokens"].map(token_positions)
+
+#         inverted_index = build_inverted_index_simple(database_pd)
+
+#         ingredient_rows = []
+#         meal_ingredient_rows = []
+#         meal_rows = []
+
+
+#         for index, row in database_pd.iterrows():
+#             meal_rows.append((
+#                 str(row["Images"]),
+#                 str(row["Name"]),
+#                 str(row["RecipeInstructions"]),
+#                 str(row["RecipeCategory"]),
+#                 str(row["Description"]),
+#                 float(row["AggregatedRating"]),
+#                 float(row["Calories"]),
+#                 int(row["RecipeServings"]),
+#                 str(row["RecipeYield"]),
+#                 int(row["hours"]),
+#                 int(row["minutes"]),
+#                 int(row["totaltime_min"]),
+#                 str(row["calories_classification"]),
+#                 str(row["MacroClassification"])
+#             ))
+
+#         await conn.executemany(
+#             """
+#             INSERT INTO meals (images, name, recipe_instructions, recipe_category, description, aggregated_rating, calories, recipe_servings, recipe_yield, hours, minutes, totaltime_min, calories_classification, macro_classification)
+#             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+#             """,
+#             meal_rows
+#         )
+
+
+#         for index, (key, value) in enumerate(inverted_index.items()):
+#             ingredient_rows.append((str(key), str(value)))
+
+#             for meal_id in value:
+#                 meal_ingredient_rows.append((int(meal_id + 1), int(index + 1), str(database_pd.loc[meal_id, "RecipeServings"])))
+
+#         await conn.executemany(
+#             """
+#             INSERT INTO ingredients (name, meals)
+#             VALUES ($1, $2)
+#             """,
+#             ingredient_rows
+#         )
+
+#         await conn.executemany(
+#             """
+#             INSERT INTO meal_ingredients (meal_id, ingredient_id, serving)
+#             VALUES ($1, $2, $3)
+#             """,
+#             meal_ingredient_rows
+#         )
+         
